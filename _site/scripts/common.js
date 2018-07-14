aa881331c3fe84462049a71c842a486a7b0b0066
Priyesh Walunj <priyeshwalunj@Priyeshs-MacBook-Air.local>
2018-07-14 23:59:33 +0530

    Form Input UI developed

--- _site/scripts/common.js
+++ _site/scripts/common.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
 
     $('.toggle-btn').on('click', function() {
         $('.sidebar').toggleClass('sidebar-show');
+        $('.responsive-logo').toggle(600)
+
     });
 
-    
-    
     var sidebarShow = $('.sidebar').hasClass('sidebar-show');
     
     if(sidebarShow) {
@@ -20,6 +20,15 @@ $(document).ready(function() {
     $('.sidebar').on('change', function() {
         console.log('content click')
     })
+
+    url = $(location).attr('href').split('/').pop().replace(/.html/,"");
+    navtext = $('.nav li a').data('value')
+    console.log(url)
+
+    if(url == navtext) {
+        console.log('in Loop')
+        $(this).addClass('active')
+    }
 
     $('.nav li a').on('click', function() {
         clickValue = $(this).text();
